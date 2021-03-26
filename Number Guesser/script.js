e1eef4343e363ace8ac5d90180026bbd3df82a31
fizzysodas <66885605+fizzysodas@users.noreply.github.com>
2021-03-26 08:55:15 -0700

    Update script.js
    done

--- Number Guesser/script.js
+++ Number Guesser/script.js
@@ -1,76 +1,119 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
-let compGuess = 0;
-let tarGuess = 0;
-let humGuess = 0;
 
 
+// Write your code below:
+let cGuess;
+let tGuess;
+let hGuess;
+let humanWon = "true";
+let roundNumber;
+
+
+const guessButton = document.getElementById('guess');
+const tarGuess = document.getElementById('target-number');
+const humanGuessInput = document.getElementById('human-guess');
+const compGuess = document.getElementById('computer-guess');
+const roundNumberDisplay = document.getElementById('round-number');
+const humanScoreDisplay = document.getElementById('human-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const computerWinsDisplay = document.getElementById('computer-wins');
+const nextRoundButton = document.getElementById('next-round')
 
 //CODE
 
-//GAME PLAY
-    //game picking winning number
-        function generateTarget() {
-            return Math.floor(Math.random() * 9);
-        }
-
-    //calling function 
-        tarGuess = generateTarget();
+//BUTTON IS CLICKED & HUMAN GUESS
     
-    //changing the HTML target-number
-        document.getElementById("target-number").innerHTML = tarGuess
-
-  
-    //This function will be called each round to determine which guess is closest to the target number.
-        function compareGuesses (tarGuess, compGuess, humGuess) {
-            //subtracting both values from target-number. abs() is absolute value
-            humGuess = Math.abs(humGuess - tarGuess);
-            compGuess = Math.abs(compGuess - tarGuess);
-
-            //min() returns the lowest-valued number passed into it. z value holds lower number
-            var z = Math.min(humGuess, compGuess);
-
-            // which value humGuess or compGuess is z value and returns true or false
-            if (humGuess === z && compGuess === z){
-                return true;
-              }else if (humGuess === z) {
-                return true;
-              }
-              else{
-                return false;
-              }
-        }
-
+    //Start game    
     
-//COMPUTER SIDE
+
+    //set to humGuess 
+    function myFunction () {
+        var hGuess = humanGuessInput.value;
+    }
+
+//COMPUTER GUESS SIDE
+    //Event listener function 
+        guessButton.addEventListener('click', () => {
+            computerPickingNumber ();
+            generateTarget();
+            let humanWon =  compareGuesses();
+            updateScore();
+            guessButton.className = "button[disabled]";
+            nextRoundButton.disabled = false;
+
+
+
+        });
+
 
     //computer picking number function
         function computerPickingNumber () {
-            return Math.floor(Math.random() * 9);
+         cGuess = Math.floor(Math.random() * 9);
+         compGuess.innerHTML = cGuess;
+    };
+
+//TARGET NUMBER
+    //game picking winning number
+        function generateTarget() {
+            tGuess = Math.floor(Math.random() * 9);
+            tarGuess.innerHTML = tGuess;
         }
 
-    //calling computerPickNumber 
-        compGuess = computerPickingNumber();
-
-    //changing the HTML computer-guess
-       document.getElementById("computer-guess").innerHTML = compGuess;
-       
-  
-       
-
-
-
-
-
-
-//HUMAN SIDE
-
-        //get value human choose
-        function humanNumber () {
-        humGuess = document.getElementById("human-guess");
-        document.getElementById("round-number").innerHTML = humGuess;
+//GAME PLAY
+    //This function will be called each round to determine which guess is closest to the target number.
+        function compareGuesses (tGuess, cGuess, hGuess) {
+    
+            if (Math.abs(hGuess - tGuess) < Math.abs(cGuess - tGuess) || hGuess === cGuess) {
+                return "true";
+            } else if(Math.abs(cGuess - tGuess) < Math.abs(hGuess - tGuess)) {
+                return "false";
+            };
         }
+    
+
+
+
+
+//This function will be used to correctly increase the winner’s score after each round.
+    function updateScore(humanWon){
+        if(humanWon == "true"){
+            humanScore += 1;
+            humanScoreDisplay.innerHTML = humanScore;
+            guessButton.innerHTML= "You Win!!!";
+            guessButton.className = "winning-text";
+
+        }else{
+            computerScore =+ 1;
+            computerScoreDisplay.innerHTML = computerScore;
+            computerWinsDisplay.innerHTML = "Computer Wins";
+            guessButton.className = "winning-text[disabled]";
+        }
+    }
+
+
+//NEXT ROUND
+    //Event listener function 
+    nextRoundButton.addEventListener('click', () => {
+        currentRoundNumber = advanceRound();
+        
+
+
+    });
+
+
+
+
+
+
+
+    function advanceRound(currentRoundNumber){
+        roundNumber = ++currentRoundNumber;
+        roundNumberDisplay.innerHTML = roundNumber;
+        currentRoundNumber = roundNumber;
+    }
+
 
      
 
